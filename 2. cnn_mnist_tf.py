f2a30ac404d3fef910be7a429e4d290f5adaaa70
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-10-26 23:53:39 +0530

    added single layer

--- 2. cnn_mnist_tf.py
+++ 2. cnn_mnist_tf.py
@@ -47,7 +47,31 @@ images = data.x_test[0:9]
 cls_true = data.y_test_cls[0:9]
 plot_images(images = images, cls_true = cls_true)
 
+def new_weights(shape):
+	return tf.Variable(tf.truncated_normal(shape, stddev = 0.05))
+def new_biases(length):
+	return tf.Variable(tf.constant(0.05, shape = [length]))
 
+def new_conv_layer(input, #previous layer input
+					num_input_channels, #of previous layer
+					filter_size, #width and height of each filter
+					num_filters,
+					use_pooling=True) #2x2 max-pooling
+	
+	shape = [filter_size, filter_size, num_input_channels, num_filters] #shape of filter-weights for convolution
+	wieghts = new_weights(shape = shape)
+	biases = new_biases(length = num_filters)
+
+	'''
+	The strides are set as 1 in all dimensions, first and last stride are labelled as 1
+	first being for image-number and last for input-channel. padding =same => size of
+	input and output image are same.
+	'''
+	layer = tf.nn.conv2d(input = input,
+							filter = weights,
+							strides = [1,1,1,1],
+							padding = 'SAME')
+	
 
 
 
