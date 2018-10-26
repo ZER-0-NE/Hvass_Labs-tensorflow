332cd818ed7578c4d20c0d149f140198005765bc
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-10-26 19:13:47 +0530

    function to plot images added

--- 2. cnn_mnist_tf.py
+++ 2. cnn_mnist_tf.py
@@ -17,3 +17,37 @@ num_filters2 = 26
 #fully-connected
 fc_size = 128
 
+img_size = data.img_size
+img_size_flat = data.img_size_flat #1-D array of data values
+img_shape = data.img_shape
+num_classes = data.num_classes
+num_channels = data.num_channels #no. of color channels for images
+
+def plot_images(images, cls_true, cls_pred = None):
+	assert len(images) == len(cls_true) == 9
+	fig, axes = plt.subplots(3,3)
+	fig.subplots_adjust(hspace = 0.3, wspace = 0.3)
+
+	for x, y in enumerate(axes.flat):
+		y.imshow(images[x].reshape(img_shape), cmap = 'binary')
+
+		if cls_pred is None:
+			xlabel = "True {0}".format(cls_true[x])
+		else:
+			xlabel = "True {0}, Pred: {1}".format(cls_true[x], cls_pred[x])
+
+		y.set_xlabel(xlabel)
+
+		y.set_xticks([])
+		y.set_yticks([])
+
+	plt.show()
+
+images = data.x_test[0:9]
+cls_true = data.y_test_cls[0:9]
+plot_images(images = images, cls_true = cls_true)
+
+
+
+
+
