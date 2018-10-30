e0376b34100b98d09c62428cf791006873685565
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-10-31 00:36:21 +0530

    added till fc layer

--- 2. cnn_mnist_tf.py
+++ 2. cnn_mnist_tf.py
@@ -59,7 +59,7 @@ def new_conv_layer(input, #previous layer input
 					use_pooling=True) #2x2 max-pooling
 	
 	shape = [filter_size, filter_size, num_input_channels, num_filters] #shape of filter-weights for convolution
-	wieghts = new_weights(shape = shape)
+	weights = new_weights(shape = shape)
 	biases = new_biases(length = num_filters)
 
 	'''
@@ -71,7 +71,75 @@ def new_conv_layer(input, #previous layer input
 							filter = weights,
 							strides = [1,1,1,1],
 							padding = 'SAME')
-	
+	layer += biases
+	if use_pooling:
+		layer = tf.nn.max_pool(value = layer,
+								ksize  = [1,2,2,1],
+								strides = [1,2,2,1],
+								padding = 'SAME')
+	# ReLu calculates max(x,0) for every pixel value x in I/P img.
+	layer = tf.nn.relu(layer)
 
+	return layer, weights
 
+def flatten_layer(layer):
+	layer_shape = layer.get_shape()
+	'''
+	shape of input layer is assumed to be [num_images, img_width, img_height, num_channels]
+	no. of features is img_width*img_height*num_channels
+	we can use a function from tensorflow to calculate this
+	'''
+	num_features = layer_shape[1:4].num_elements()
+	'''
+	reshape the layer to [num_images, num_features]
+	we set the size of second dimension to num_features and first dimension to 
+	-1. 
+	'''
+	layer_flat = tf.reshape(layer, [-1, num_features])
+	#the shape of flattened layer is now [num_images, img_height*img_width*num_channels]
+
+	return layer_flat, num_features
+
+def new_fc_layer(input, # previous layer
+				num_inputs, #of prev layer
+				num_outputs,
+				use_relu = True)
+	weights = new_weights(shape = [num_inputs,  num_outputs])
+	biases = new_biases(length = num_outputs)
+
+	layer = tf.matmul(input, weights) + biases
+	if use_relu:
+		layer = tf.nn.relu(layer)
+
+	return layer
+
+x = tf.placeholder(tf.float32, shape = [None, img_size_flat], name = 'x')
+x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+y_true = tf.placeholder(tf.float32, shape = [None, num_classes], name = 'y_true')
+y_true_cls = tf.argmax(y_true, axis = 1)
+
+#Conv layer 1
+layer_conv1, weights_conv1 = new_conv_layer(
+							input = x_image,
+							num_input_channels = num_channels,
+							filter_size = filter_size1,
+							num_filters = num_filters1,
+							use_pooling = True)
+
+layer_conv2, weights_conv2 = new_conv_layer(
+							input = layer_conv1,
+							num_input_channels = num_filters1,
+							filter_size = filter_size2,
+							num_filters = num_filters2,
+							use_pooling = True)
+
+#Flattening
+layer_flat, num_features = flatten_layer(layer_conv2)
+
+#FC layer
+layer_fc2 = new_fc_layer(
+						input = layer_fc1,
+						num_inputs = fc_size,
+						num_outputs = num_classes,
+						use_relu = False)
 
