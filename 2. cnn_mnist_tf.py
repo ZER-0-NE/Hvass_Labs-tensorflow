af2302d7692ce6ff50133078dbd4a90aabb1612e
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-12-08 01:25:10 +0530

    added sentiment_analysis_rnn.py

--- 2. cnn_mnist_tf.py
+++ 2. cnn_mnist_tf.py
@@ -36,54 +36,52 @@ def plot_images(images, cls_true, cls_pred = None):
 		else:
 			xlabel = "True {0}, Pred: {1}".format(cls_true[x], cls_pred[x])
 
-		y.set_xlabel(xlabel)
+			y.set_xlabel(xlabel)
 
-		y.set_xticks([])
-		y.set_yticks([])
+			y.set_xticks([])
+			y.set_yticks([])
 
-	plt.show()
+			plt.show()
 
-images = data.x_test[0:9]
-cls_true = data.y_test_cls[0:9]
-plot_images(images = images, cls_true = cls_true)
+			images = data.x_test[0:9]
+			cls_true = data.y_test_cls[0:9]
+			plot_images(images = images, cls_true = cls_true)
 
-def new_weights(shape):
-	return tf.Variable(tf.truncated_normal(shape, stddev = 0.05))
-def new_biases(length):
-	return tf.Variable(tf.constant(0.05, shape = [length]))
+			def new_weights(shape):
+				return tf.Variable(tf.truncated_normal(shape, stddev = 0.05))
+				def new_biases(length):
+					return tf.Variable(tf.constant(0.05, shape = [length]))
 
 def new_conv_layer(input, #previous layer input
 					num_input_channels, #of previous layer
 					filter_size, #width and height of each filter
 					num_filters,
-					use_pooling=True) #2x2 max-pooling
-	
-	shape = [filter_size, filter_size, num_input_channels, num_filters] #shape of filter-weights for convolution
+					use_pooling = True)
+	shape = [filter_size, filter_size, num_input_channels, num_filters]#shape of filter-weights for convolution
 	weights = new_weights(shape = shape)
 	biases = new_biases(length = num_filters)
-
 	'''
 	The strides are set as 1 in all dimensions, first and last stride are labelled as 1
 	first being for image-number and last for input-channel. padding =same => size of
 	input and output image are same.
 	'''
 	layer = tf.nn.conv2d(input = input,
-							filter = weights,
-							strides = [1,1,1,1],
-							padding = 'SAME')
+		filter = weights,
+		strides = [1,1,1,1],
+		padding = 'SAME')
 	layer += biases
 	if use_pooling:
 		layer = tf.nn.max_pool(value = layer,
-								ksize  = [1,2,2,1],
-								strides = [1,2,2,1],
-								padding = 'SAME')
+			ksize  = [1,2,2,1],
+			strides = [1,2,2,1],
+			padding = 'SAME')
 	# ReLu calculates max(x,0) for every pixel value x in I/P img.
 	layer = tf.nn.relu(layer)
 
 	return layer, weights
 
-def flatten_layer(layer):
-	layer_shape = layer.get_shape()
+	def flatten_layer(layer):
+		layer_shape = layer.get_shape()
 	'''
 	shape of input layer is assumed to be [num_images, img_width, img_height, num_channels]
 	no. of features is img_width*img_height*num_channels
@@ -104,45 +102,87 @@ def new_fc_layer(input, # previous layer
 				num_inputs, #of prev layer
 				num_outputs,
 				use_relu = True)
-	weights = new_weights(shape = [num_inputs,  num_outputs])
-	biases = new_biases(length = num_outputs)
+weights = new_weights(shape = [num_inputs,  num_outputs])
+biases = new_biases(length = num_outputs)
 
-	layer = tf.matmul(input, weights) + biases
-	if use_relu:
-		layer = tf.nn.relu(layer)
+layer = tf.matmul(input, weights) + biases
+if use_relu:
+	layer = tf.nn.relu(layer)
 
 	return layer
 
-x = tf.placeholder(tf.float32, shape = [None, img_size_flat], name = 'x')
-x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
-y_true = tf.placeholder(tf.float32, shape = [None, num_classes], name = 'y_true')
-y_true_cls = tf.argmax(y_true, axis = 1)
+	x = tf.placeholder(tf.float32, shape = [None, img_size_flat], name = 'x')
+	x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
+	y_true = tf.placeholder(tf.float32, shape = [None, num_classes], name = 'y_true')
+	y_true_cls = tf.argmax(y_true, axis = 1)
 
 #Conv layer 1
 layer_conv1, weights_conv1 = new_conv_layer(
-							input = x_image,
-							num_input_channels = num_channels,
-							filter_size = filter_size1,
-							num_filters = num_filters1,
-							use_pooling = True)
+	input = x_image,
+	num_input_channels = num_channels,
+	filter_size = filter_size1,
+	num_filters = num_filters1,
+	use_pooling = True)
 
 layer_conv2, weights_conv2 = new_conv_layer(
-							input = layer_conv1,
-							num_input_channels = num_filters1,
-							filter_size = filter_size2,
-							num_filters = num_filters2,
-							use_pooling = True)
+	input = layer_conv1,
+	num_input_channels = num_filters1,
+	filter_size = filter_size2,
+	num_filters = num_filters2,
+	use_pooling = True)
 
 #Flattening
 layer_flat, num_features = flatten_layer(layer_conv2)
 
 #FC layer
 layer_fc2 = new_fc_layer(
-						input = layer_fc1,
-						num_inputs = fc_size,
-						num_outputs = num_classes,
-						use_relu = False)
+	input = layer_fc1,
+	num_inputs = fc_size,
+	num_outputs = num_classes,
+	use_relu = False)
 
 # Normalizing the values so that they occur between zero and one and all the elements sum to 10
 y_pred = tf.nn.softmax(layer_fc2)
 y_pred_cls = tf.argmax(y_pred, axis = 1)
+
+#optimizing cost function
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits = layer_fc2,
+	labels = y_true)
+#avg of the cross-entropy of all the image-classifications
+cost = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.AdamOptimizer(learning_rate = 1e-4).minimize(cost)
+
+correct_prediction = tf.equal(y_pred_cls,y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+session = tf.Session()
+
+session.run(tf.global_variables_initializer())
+
+train_batch_size = 12
+
+total_iterations = 0
+
+def optimize(num_iterations):
+	global num_iterations
+
+	start_time = time.time()
+	for i in range(total_iterations, total_iterations+num_iterations):
+		x_batch, y_true_batch = data.random(batch_size = train_batch_size)
+
+		feed_dict_train = {x: x_batch,
+		y_true: y_true_batch}
+
+		session.run(optimizer, feed_dict = feed_dict_train)
+
+		if i % 100 == 0:
+			acc = session.run(accuracy, feed_dict = feed_dict_train)
+			msg = "Optimization iteration: {0:6}, Training accuracy: {0:6.1%}"
+
+			print(msg.format(i+1, acc))
+
+			total_iterations += num_iterations
+			end_time = time.time()
+			time_diff = end_time - start_time
+			print("Time usage: " + str(timedelta(seconds = int(round(time_diff)))))
---
+++ 3. sentiment_analysis_imdb_data_rnn.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from scipy.spatial.distance import cdist
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, GRU, Embedding
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+
+import imdb
+#imdb.maybe_download_and_extract()
+
+x_train_text, y_train = imdb.load_data(train=True)
+x_test_text, y_test = imdb.load_data(train=False)
+
+# print("Train-set size: ", len(x_train_text)) # 25k
+# print("Test-set size:  ", len(x_test_text)) # 25k
+data_text = x_train_text + x_test_text
+
+num_words = 10000
+tokenizer = Tokenizer(num_words = num_words)
+tokenizer.fit_on_texts(data_text)
+
+if num_words is None:
+    num_words = len(tokenizer.word_index)
+
+# converting all texts to corresponding tokens
+x_train_tokens = tokenizer.texts_to_sequences(x_train_text)
+x_test_tokens = tokenizer.texts_to_sequences(x_test_text)
+
+# allowing fixed size tokens 
+num_tokens = [len(tokens) for tokens in x_train_tokens + x_test_tokens]
+num_tokens = np.array(num_tokens)
+
+# The max number of tokens we will allow is set to the average plus 2 standard deviations.
+max_tokens = np.mean(num_tokens) + 2 * np.std(num_tokens)
+max_tokens = int(max_tokens)
+
+# pre-padding
+pad = 'pre'
+x_train_pad = pad_sequences(x_train_tokens, maxlen=max_tokens,
+                            padding=pad, truncating=pad)
+x_test_pad = pad_sequences(x_test_tokens, maxlen=max_tokens,
+                           padding=pad, truncating=pad)
